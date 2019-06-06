477fb3d4a559e051e534394821f55aaf22eb65ec
Brent de Kok <brent@worth.systems>
2019-06-06 15:11:40 +0200

    Updated package.json and demo (untested)

---
+++ demo/precache-manifest.e550b3d2c759bb88589716da44d909c5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10b13440d49b64038a4401e6692d0328",
+    "url": "/bluetooth-lights/demo/index.html"
+  },
+  {
+    "revision": "06893eca98102ff116b3",
+    "url": "/bluetooth-lights/demo/static/css/main.1ec88ff0.chunk.css"
+  },
+  {
+    "revision": "1d8dbd141f0df92c9485",
+    "url": "/bluetooth-lights/demo/static/js/2.573f9ea2.chunk.js"
+  },
+  {
+    "revision": "06893eca98102ff116b3",
+    "url": "/bluetooth-lights/demo/static/js/main.28e5a060.chunk.js"
+  },
+  {
+    "revision": "99dccd835d8d4af8b07f",
+    "url": "/bluetooth-lights/demo/static/js/runtime~main.b814a56a.js"
+  }
+]);
